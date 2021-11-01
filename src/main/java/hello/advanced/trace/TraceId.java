2bdefc13609e7754e53dd1f1280c225d7fb3be56
weekbelt <vfrvfr4207@gmail.com>
2021-11-01 20:40:29 +0900

    로그 추적기 V2 - 파라미터로 동기화 개발

--- src/main/java/hello/advanced/trace/TraceId.java
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -21,7 +21,7 @@ public class TraceId {
         return UUID.randomUUID().toString().substring(0, 8);
     }
 
-    private TraceId createNextId() {
+    public TraceId createNextId() {
         return new TraceId(id, level + 1);
     }
 
